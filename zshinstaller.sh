e8049261d131bc2c6074119bef86541bc762b117
logm <loganmarkle@gmail.com>
2020-07-01 17:06:46 -0700

    updated installer helper script

--- zshinstaller.sh
+++ zshinstaller.sh
@@ -1,17 +1,16 @@
-sudo apt install -y wget
+sudo apt install -y curl
 sudo apt install -y git
+
 
 
 echo "---------installing zsh---------"
 sudo apt install -y zsh
 echo "---------changing default shell---------"
 chsh -s $(which zsh)
-echo "---------installing ohmyzsh---------"
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "---------installing p10k---------"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-echo "---------install plugins---------"
-echo "---------installing autosuggestions---------"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "---------install autoenv---------"
-git clone git://github.com/inishchith/autoenv.git ~/.autoenv
+echo "---------installing antigen---------"
+curl -L git.io/antigen > antigen.zsh
+echo "---------modifying rc files---------"
+echo "source $HOME/dev-config/logan.vimrc" > $HOME/.vimrc
+echo "source $HOME/dev-config/logan.antigenrc" > $HOME/.antigenrc
+echo "source $HOME/dev-config/logan.p10k.zsh" > $HOME/.p10k.zsh
+echo "source $HOME/dev-config/logan.zshrc" > $HOME/.zshrc
